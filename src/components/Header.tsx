c96421e0e687035fb9c868afaa3c0e367f769c06
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-12-18 15:08:32 +0100

    chore: changed header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,16 @@ const Header: React.FC = () => {
     <header className='max-h-[10vh] max-w-[144rem] mx-auto p-4 py-3 text-xl border-b-1 border-blue-500 border-opacity-20'>
       <Link
         href='/'
-        className={`${redHat.className} w-[15rem] flex flex-col gap-1 uppercase tracking-widest`}
+        className={`${redHat.className} max-w-fit flex flex-col gap-1 uppercase tracking-widest`}
       >
-        <span className='leading-8'>Daniel</span>
+        <span className='block bg-gradient-linear-100 bg-clip-text leading-8 font-semibold text-transparent'>
+          D. B.
+        </span>
         {/* <hr className='w-full' /> */}
-        <div className='h-[0.1rem] bg-gray-100'></div>
-        <span className='w-full leading-8 text-right'>Ezekiel</span>
+        <div className='h-[0.1rem] bg-gradient-linear-100'></div>
+        <span className='block bg-gradient-linear-100 bg-clip-text leading-8 text-transparent font-semibold text-center'>
+          E.
+        </span>
       </Link>
 
       <ul className='hidden'>
